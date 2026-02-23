68096813125cae44bf88214c9f7b295c66864487
maksimpilitsyn <maksim_pilitsyn@unitalk.cloud>
2026-02-23 20:40:28 +0000

    Remove network resource to bypass API 1.41 error

--- main.tf
+++ main.tf
@@ -11,19 +11,27 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
+resource "docker_container" "backend" {
+  name  = "twitter-deploy-backend-1"
+  image = "twitter-deploy-backend:latest"
+  restart = "always"
+  
+  networks_advanced {
+    name = "app-network"
+  }
+}
+
 resource "docker_container" "frontend" {
   name  = "twitter-deploy-frontend-1"
   image = "twitter-deploy-frontend:latest"
+  restart = "always"
   
   ports {
     internal = 80
     external = 8082
   }
 
-  # Просто указываем имя, не ссылаясь на ресурс Terraform
   networks_advanced {
     name = "app-network"
   }
 }
-
-# Если есть backend, сделай аналогично для него
