3ff41ae381969e593203b6e74b41490b1689eb02
maksimpilitsyn <maksim_pilitsyn@unitalk.cloud>
2026-02-23 20:51:10 +0000

    Hardcode Docker API version to 1

--- main.tf
+++ main.tf
@@ -10,3 +10,28 @@ terraform {
 provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
+
+resource "docker_container" "backend" {
+  name  = "twitter-deploy-backend-1"
+  image = "twitter-deploy-backend:latest"
+  restart = "always"
+  
+  networks_advanced {
+    name = "app-network"
+  }
+}
+
+resource "docker_container" "frontend" {
+  name  = "twitter-deploy-frontend-1"
+  image = "twitter-deploy-frontend:latest"
+  restart = "always"
+  
+  ports {
+    internal = 80
+    external = 8082
+  }
+
+  networks_advanced {
+    name = "app-network"
+  }
+}
